7e052edfc5c3d9d29f47ebd5a2916b2cbb66cdc4
Honceriu Emilian-Andrei <emilian.honceriu@gmail.com>
2024-01-14 15:40:05 +0200

    added initial login configuration

---
+++ src/app/(login)/layout.tsx
@@ -0,0 +1,20 @@
+"use client";
+import "../globals.css";
+import { usePathname } from "next/navigation";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+  return (
+    <html lang="en">
+      <body>
+        <main className="flex min-h-screen w-full gap-4 bg-gray-200 items-center justify-between p-4">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
