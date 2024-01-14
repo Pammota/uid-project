c384479998287ebae6221591d3d37d5a7042d70b
Honceriu Emilian-Andrei <emilian.honceriu@gmail.com>
2024-01-14 12:03:02 +0200

    added founds input fields

--- src/app/children-founds/page.tsx
+++ src/app/children-founds/page.tsx
@@ -1,9 +1,41 @@
-import React from "react";
+"use client"
+import React, {useState} from "react";
+
+type FoundProps = {
+    name: string;
+    amount: number;
+}
+
+export const Found = ({name, amount} : FoundProps) => {
+    
+    const [currentAmount, setAmount] = useState<number | null>(amount);
+
+    const handleAmountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newAmount = parseFloat(event.target.value);
+        setAmount(isNaN(newAmount) ? null : newAmount);
+    }
+
+    return (
+        <div className="w-full bg-yellow-400 rounded-lg p-2 flex flex-col gap-2 overflow-y-auto items-start">
+            <label className="text-black text-xl">
+                {name}
+            </label>
+            <input
+                type="number"
+                value={currentAmount !== null ? currentAmount : ''}
+                onChange={handleAmountChange}
+                className="border rounded py-2 px-3 w-1/2 self-start"
+            />
+        </div>
+    )
+
+}
 
 export default function Page() {
     return (
-        <div>
-            Alo
+        <div className="w-full bg-blue-400 rounded-lg p-2 flex flex-col gap-2 overflow-y-auto items-center">
+            <Found name="Monthly Budget" amount={100}/>
+            <Found name="Monthly Income" amount={100}/>
         </div>
     )
 }
